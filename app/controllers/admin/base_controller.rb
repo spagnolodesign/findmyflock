518004beeab644742bf8869f74a50b1bc8c0992f
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-06-05 15:27:41 +0800

    moved auth to routes for admin session

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,3 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
-  # before_filter :require_login
-
-  private
-  def not_authenticated
-    redirect_to login_path, alert: "Please login first"
-  end
 end
