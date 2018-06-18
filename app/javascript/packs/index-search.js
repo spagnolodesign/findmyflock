3901a4130b425ac1cdf1294d81ce2b3a42182e54
Sonia Montero <sonia.montero@skema.edu>
2018-06-18 17:01:23 +0800

    fixed dropdown and dev dash filters + details

--- app/javascript/packs/index-search.js
+++ app/javascript/packs/index-search.js
@@ -12,25 +12,6 @@ const formRequest = () => {
   });
 }
 
-const filterToggle = () => {
-  let cards = document.querySelectorAll(".filter-card")
-  if (document.querySelector("#dashboard-container") != null) {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].childNodes[1].addEventListener("click", function() {
-        let collapse = cards[i].childNodes[3]
-        if (collapse.classList.contains("show")) {
-          collapse.classList.remove("show")
-        } else {
-          collapse.classList.add("show")
-        }
-      })
-    }
-  }
-}
-
 export {
   formRequest
-}
-export {
-  filterToggle
 }
