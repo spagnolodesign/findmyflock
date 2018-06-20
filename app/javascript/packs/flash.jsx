17f26d02aa0a122f69b0cbc7e1db0b321b79e280
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-06-20 15:54:32 +0800

    Flash alert setup

---
+++ app/javascript/packs/flash.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+export class Flash extends Component {
+ constructor(props) {
+    super();
+    this.state = {
+      show:true,
+      type: (!props.alert) ? "alert-success" : "alert-danger"
+    }
+ }
+
+ componentDidMount(){
+   setTimeout(function() { this.setState({show: false}); }.bind(this), 5000);
+ }
+
+ render(){
+   const {show, type} = this.state
+   if (!show){
+    return;
+   }
+   return(
+     <div className={`alert alert-primary ${type} mt-3`} role="alert">
+        <span>{this.props.alert || this.props.notice}</span>
+     </div>
+   )
+ }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.getElementById("alert-component");
+
+  if (el != null) {
+    const alert_txt = el.getAttribute('data-alert');
+    const notice_txt = el.getAttribute('data-notice');
+    ReactDOM.render(
+      <Flash alert={alert_txt} notice={notice_txt} />, el,
+    )
+  }
+})
