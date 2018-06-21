f6495f55f2f831b30045ed4bb7555917a0cca036
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-06-21 13:07:44 +0800

    edit seed

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -85,14 +85,7 @@ class Developer < ApplicationRecord
     end
   end
 
-  def self.check_for_first_matches
-    all.each do |developer|
-      developer.matched_job.each do |job|
-        Match.create(developer_id: developer.id, job_id: job.id)
-      end
-    end
-  end
-  
+
   def check_for_first_matches
     self.matched_job.each do |job|
       Match.create(developer_id: self.id, job_id: job.id)
