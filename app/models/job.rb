af0a90ed8f4b7477909514f19f77354a6c8866d7
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-06-29 08:55:53 +0800

    Skills generator and remove limit on benefit job validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -10,7 +10,7 @@ class Job < ApplicationRecord
   validates :max_salary, numericality: { only_integer: true, greater_than: 0}, :allow_nil => true
   validates :remote, inclusion: { in: [["remote"], ["office"], ["remote", "office"]]}
   validates :employment_type, presence: true, length: { maximum: 100 }
-  validates :benefits, :cultures, length: { minimum: 1 }, on: :update
+  validates :benefits, length: { minimum: 1 }, on: :update
   geocoded_by :location
   before_validation :geocode
   validate :check_cordinates, on: [:create, :update]
